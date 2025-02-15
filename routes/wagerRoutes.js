5508cc2e1e1b2b0aec085645083aa031913c046f
Timi16 <timilehinolowu46@gmail.com>
2025-02-15 01:18:34 -0800

    adding wager routes

--- routes/wagerRoutes.js
+++ routes/wagerRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const wagerController = require('../controllers/wagerController'); // Ensure correct path
 
-// ✅ Wager Routes
+
 router.post('/create', wagerController.createWager);
 router.post('/join', wagerController.joinWager);
 router.post('/declare', wagerController.declareOutcome);
-router.get('/all', wagerController.getAllWagers); // ✅ New route to get all wagers
+router.get('/all', wagerController.getAllWagers); 
 
 module.exports = router;
